ddf599921cad60fba9837c41437d881bb06b64ef
mostafa-semnani-74 <mostafa.semnani.74@gmail.com>
2024-03-14 16:54:36 +0330

    init project init config server init discovery server init gateway init product microservice

---
+++ config-server/src/main/java/ir/mostafa/semnani/configserver/ConfigServerApplication.java
@@ -0,0 +1,13 @@
+package ir.mostafa.semnani.configserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConfigServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConfigServerApplication.class, args);
+	}
+
+}
---
+++ config-server/src/test/java/ir/mostafa/semnani/configserver/ConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package ir.mostafa.semnani.configserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
