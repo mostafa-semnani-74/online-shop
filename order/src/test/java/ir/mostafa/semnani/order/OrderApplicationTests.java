ab757f502cd86a660b103b7d284265aab7701bd0
mostafa-semnani-74 <mostafa.semnani.74@gmail.com>
2024-06-20 19:28:37 +0330

    add order service package

---
+++ order/src/test/java/ir/mostafa/semnani/order/OrderApplicationTests.java
@@ -0,0 +1,13 @@
+package ir.mostafa.semnani.order;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
