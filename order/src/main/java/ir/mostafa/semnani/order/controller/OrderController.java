d2952b1da8cdbeda8c3c1a04f6a0101505851b62
mostafa-semnani-74 <mostafa.semnani.74@gmail.com>
2024-06-25 15:17:37 +0330

    add resilience4j circuit breaker for save in order service

--- order/src/main/java/ir/mostafa/semnani/order/controller/OrderController.java
+++ order/src/main/java/ir/mostafa/semnani/order/controller/OrderController.java
@@ -1,5 +1,6 @@
 package ir.mostafa.semnani.order.controller;
 
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import ir.mostafa.semnani.order.dto.OrderDTO;
 import ir.mostafa.semnani.order.service.OrderService;
 import lombok.RequiredArgsConstructor;
@@ -16,6 +17,7 @@ public class OrderController {
     private final OrderService orderService;
 
     @PostMapping
+    @CircuitBreaker(name = "save-order", fallbackMethod = "saveFallback")
     public ResponseEntity<OrderDTO> save(@RequestBody OrderDTO orderDTO) {
         return new ResponseEntity<>(orderService.save(orderDTO), HttpStatus.CREATED);
     }
@@ -23,6 +25,10 @@ public class OrderController {
     @GetMapping
     public ResponseEntity<List<OrderDTO>> findAll() {
         return ResponseEntity.ok(orderService.findAll());
+    }
+
+    private ResponseEntity<String> saveFallback(Exception e) {
+        return new ResponseEntity<>("save order is done , please try later !!!!!", HttpStatus.INTERNAL_SERVER_ERROR);
     }
 
 }
